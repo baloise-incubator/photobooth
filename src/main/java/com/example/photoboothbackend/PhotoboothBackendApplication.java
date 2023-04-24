38e03574689d491ffe84c892fa4748ae30b69177
lazaki <lazar.rk@gmail.com>
2023-04-24 12:30:06 +0200

    Remove java template

---
+++ src/main/java/com/example/photoboothbackend/PhotoboothBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoboothbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoboothBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoboothBackendApplication.class, args);
+	}
+
+}
