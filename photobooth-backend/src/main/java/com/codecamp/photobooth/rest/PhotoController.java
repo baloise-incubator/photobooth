666aba49bb8006c3cb2b577b4f5eda7b334f02f7
Predrag Arsic <predrag87arsic@gmail.com>
2023-04-25 16:44:06 +0200

    try

--- photobooth-backend/src/main/java/com/codecamp/photobooth/rest/PhotoController.java
+++ photobooth-backend/src/main/java/com/codecamp/photobooth/rest/PhotoController.java
@@ -3,6 +3,7 @@ package com.codecamp.photobooth.rest;
 
 import com.codecamp.photobooth.domain.OcrResult;
 import com.codecamp.photobooth.service.OcrService;
+import net.sourceforge.tess4j.TesseractException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class PhotoController {
 
     @Autowired
     private OcrService ocrService;
 
-    @PostMapping(value = "api/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String uploadPhoto(@RequestPart("file") MultipartFile file) {
-        OcrResult ocrResult = ocrService.ocr(file);
-        return ocrResult.getResult();
+    @PostMapping(value = "api/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String uploadPhoto(@RequestPart("file") MultipartFile file) throws IOException, TesseractException {
+
+        return ocrService.doOcr(file);
     }
 }
