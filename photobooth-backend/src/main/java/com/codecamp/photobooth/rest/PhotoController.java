1edd7262ae0da500bf8732f4452ce1944184f109
arsic994 <arsic994@gmail.com>
2023-04-26 12:32:52 +0200

    refactoring, enhancing

--- photobooth-backend/src/main/java/com/codecamp/photobooth/rest/PhotoController.java
+++ photobooth-backend/src/main/java/com/codecamp/photobooth/rest/PhotoController.java
@@ -1,29 +1,39 @@
 package com.codecamp.photobooth.rest;
 
-
-import com.codecamp.photobooth.domain.OcrResult;
 import com.codecamp.photobooth.service.OcrService;
-import net.sourceforge.tess4j.TesseractException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.util.logging.Logger;
 
 @RestController
 public class PhotoController {
 
+    static Logger log = Logger.getLogger(PhotoController.class.getName());
+
     @Autowired
     private OcrService ocrService;
 
-    @PostMapping(value = "api/upload",
+    @PostMapping(value = "api/validate",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.TEXT_PLAIN_VALUE)
-    public String uploadPhoto(@RequestPart("file") MultipartFile file) throws IOException, TesseractException {
+    public ResponseEntity<String> validateFromPhoto(@RequestPart("file") MultipartFile file, @RequestParam(required = false) String lang) {
+        log.info("OCR starting for validation process");
+        return new ResponseEntity<>(ocrService.doOcr(file, lang), HttpStatus.OK);
+    }
 
-        return ocrService.doOcr(file);
+    @PostMapping(value = "api/populate",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> populateFromPhoto(@RequestPart("file") MultipartFile file, @RequestParam(required = false) String lang) {
+        log.info("OCR starting for form population process");
+        return new ResponseEntity<>(ocrService.doOcr(file, lang), HttpStatus.OK);
     }
 }
--- photobooth-backend/src/main/java/com/codecamp/photobooth/service/OcrService.java
+++ photobooth-backend/src/main/java/com/codecamp/photobooth/service/OcrService.java
@@ -1,9 +1,9 @@
 package com.codecamp.photobooth.service;
 
-import com.codecamp.photobooth.domain.OcrResult;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,145 +12,106 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
 public class OcrService {
+
+//    @Autowired
+//    private ApplicationContext applicationContext;
+
     @Autowired
     private Tesseract tesseract;
 
-    public OcrResult ocr(MultipartFile file) {
+//    private void initTesseract(String lang) {
+//        Tesseract tess = applicationContext.getBean(Tesseract.class);
+//        tess.setLanguage(lang == null ? "deu" : lang);
+//        tesseract = tess;
+//    }
+
+    /**
+     * Initialize Tesseract, preprocess image, and executes OCR. In case of an issue Runtime is thrown.
+     *
+     * @param file     picture to be processed
+     * @param language language of the characters
+     * @return OCR string from document
+     */
+    public String doOcr(MultipartFile file, String language) {
+//        initTesseract(language);
         try {
-            File convFile = convert(file);
-            String text = tesseract.doOCR(convFile);
-            OcrResult ocrResult = new OcrResult();
-            ocrResult.setResult(text);
-            return ocrResult;
+            BufferedImage preProcessedImage = RGBToGrayScale(file);
+            String str = tesseract.doOCR(preProcessedImage);
+            return trimSpecialCharacters(str);
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
+            throw new RuntimeException("There was an issue with OCR, please try again or take different picture");
         }
     }
 
-    public String doOcr(MultipartFile file) throws IOException, TesseractException {
-        return RGBToGrayScale(file);
+    private String trimSpecialCharacters(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9\\s\\n.‘?|\"',\\/@]+", "");
+        return s;
     }
 
-    public static File convert(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        convFile.createNewFile();
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
-    }
-
-
-    public String RGBToGrayScale(MultipartFile multipartFile) throws IOException, TesseractException {
+    private BufferedImage RGBToGrayScale(MultipartFile multipartFile) throws IOException, TesseractException {
         BufferedImage ipimage = ImageIO.read(multipartFile.getInputStream());
-        double d
-                = ipimage
-                .getRGB(ipimage.getTileWidth() / 2,
-                        ipimage.getTileHeight() / 2);
-
+        double d = ipimage.getRGB(ipimage.getTileWidth() / 2, ipimage.getTileHeight() / 2);
 
         if (d >= -1.4211511E7 && d < -7254228) {
             return processImg(ipimage, 3f, -10f);
-        }
-        else if (d >= -7254228 && d < -2171170) {
+        } else if (d >= -7254228 && d < -2171170) {
             return processImg(ipimage, 1.455f, -47f);
-        }
-        else if (d >= -2171170 && d < -1907998) {
+        } else if (d >= -2171170 && d < -1907998) {
             return processImg(ipimage, 1.35f, -10f);
-        }
-        else if (d >= -1907998 && d < -257) {
+        } else if (d >= -1907998 && d < -257) {
             return processImg(ipimage, 1.19f, 0.5f);
-        }
-        else if (d >= -257 && d < -1) {
+        } else if (d >= -257 && d < -1) {
             return processImg(ipimage, 1f, 0.5f);
-        }
-        else if (d >= -1 && d < 2) {
+        } else if (d >= -1 && d < 2) {
             return processImg(ipimage, 1f, 0.35f);
         }
-        return "asdasd";
+        throw new RuntimeException("Image is out of the scale");
     }
 
-    public static String processImg(BufferedImage ipimage,
-               float scaleFactor,
-               float offset)
-            throws IOException, TesseractException
-    {
-        // Making an empty image buffer
-        // to store image later
-        // ipimage is an image buffer
-        // of input image
-        BufferedImage opimage
-                = new BufferedImage(1050,
-                1024,
-                ipimage.getType());
+    private BufferedImage processImg(BufferedImage ipimage, float scaleFactor, float offset) throws IOException, TesseractException {
+        // Making an empty image buffer to store image later ipimage is an image buffer of input image
+        BufferedImage opimage = new BufferedImage(2000, 3000, ipimage.getType());
 
-        // creating a 2D platform
-        // on the buffer image
-        // for drawing the new image
-        Graphics2D graphic
-                = opimage.createGraphics();
+        // creating a 2D platform on the buffer image for drawing the new image
+        Graphics2D graphic = opimage.createGraphics();
 
-        // drawing new image starting from 0 0
-        // of size 1050 x 1024 (zoomed images)
+        // drawing new image starting from 0 0 of size 1050 x 1024 (zoomed images)
         // null is the ImageObserver class object
-        graphic.drawImage(ipimage, 0, 0,
-                1050, 1024, null);
+        graphic.drawImage(ipimage, 0, 0, 2000, 3000, null);
         graphic.dispose();
 
-        // rescale OP object
-        // for gray scaling images
-        RescaleOp rescale
-                = new RescaleOp(scaleFactor, offset, null);
+        // rescale OP object for gray scaling images
+        RescaleOp rescale = new RescaleOp(scaleFactor, offset, null);
 
-        // performing scaling
-        // and writing on a .png file
-        BufferedImage fopimage
-                = rescale.filter(opimage, null);
-
+        // performing scaling and writing on a .png file
+        BufferedImage fopimage = rescale.filter(opimage, null);
 
         //rgb to c&w
         int width = fopimage.getWidth();
         int height = fopimage.getHeight();
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
-                int p = fopimage.getRGB(x,y);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = fopimage.getRGB(x, y);
 
-                int a = (p>>24)&0xff;
-                int r = (p>>16)&0xff;
-                int g = (p>>8)&0xff;
-                int b = p&0xff;
+                int a = (p >> 24) & 0xff;
+                int r = (p >> 16) & 0xff;
+                int g = (p >> 8) & 0xff;
+                int b = p & 0xff;
 
                 //calculate average
-                int avg = (r+g+b)/3;
+                int avg = (r + g + b) / 3;
 
                 //replace RGB value with avg
-                p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+                p = (a << 24) | (avg << 16) | (avg << 8) | avg;
 
                 fopimage.setRGB(x, y, p);
             }
         }
-        ImageIO
-                .write(fopimage,
-                        "jpg",
-                        new File("output.png"));
-
-        // Instantiating the Tesseract class
-        // which is used to perform OCR
-        Tesseract it = new Tesseract();
-
-
-        // doing OCR on the image
-        // and storing result in string str
-        String str = it.doOCR(fopimage);
-        System.out.println(str);
-
-        return str;
+        ImageIO.write(fopimage, "jpg", new File("output.png"));
+        return fopimage;
     }
-
-
 }
