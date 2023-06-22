0f485fd240fb6d74446e20ff68785d2926ffdf81
Lazar Djurdjevic <lazar.djurdjevic@baloise.ch>
2023-06-22 07:55:19 +0200

    change resources path

--- photobooth-backend/src/main/java/com/codecamp/photobooth/PhotoboothBackendApplication.java
+++ photobooth-backend/src/main/java/com/codecamp/photobooth/PhotoboothBackendApplication.java
@@ -3,10 +3,12 @@ package com.codecamp.photobooth;
 import com.codecamp.photobooth.rest.PhotoController;
 import net.sourceforge.tess4j.TessAPI;
 import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.util.LoadLibs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 @SpringBootApplication
@@ -14,12 +16,12 @@ public class PhotoboothBackendApplication {
 
     static Logger log = Logger.getLogger(PhotoController.class.getName());
 
-    public static void main(String[] args) {
-        SpringApplication.run(PhotoboothBackendApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(PhotoboothBackendApplication.class, args);}
 
     @Bean
     Tesseract getTesseract() {
+        File tmpFolder = LoadLibs.extractTessResources("linux-x86-64"); // replace platform
+        System.setProperty("java.library.path", tmpFolder.getPath());
         ClassLoader classLoader = getClass().getClassLoader();
         String resourcePath = classLoader.getResource("").getPath();
         String tessdataPath = "";
